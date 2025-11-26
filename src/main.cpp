92f2896316728788283ccbd28b2a3a460d0c4aa8
kreott <elias.ohlsson@elev.ga.ntig.se>
2025-11-26 21:35:29 +0100

    commit 4

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,13 @@
 #include <fstream>
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <cstring>
 
-// class for checking if the game is on the launch screen or not
-enum class GameState
+// structure for commands and values
+struct command 
 {
-    LaunchScreen,
-    Playing
+    sf::String text;
+    int SectionNumber;
 };
 
 // function for reading the text file and parsing the important text
@@ -17,6 +18,11 @@ sf::Text getText(std::ifstream &textFile, const std::string &targetSection, sf::
     std::string line;
     std::string result;
 
+    // resets the file so it can search from the beginning again
+    textFile.clear();                  // clear end of file tag
+    textFile.seekg(0, std::ios::beg);  // restart
+
+
     while (getline(textFile, line))
     {   
         // checks for the brackets / headers in the .txt file
@@ -24,7 +30,7 @@ sf::Text getText(std::ifstream &textFile, const std::string &targetSection, sf::
         {
             std::string sectionName = line.substr(1, line.size() - 2);
             insideSection = (sectionName == targetSection);
-            continue; // skip printing the header itself
+            continue; // skip printing the header
         }
 
         // only print lines if inside the section we want
@@ -40,8 +46,17 @@ sf::Text getText(std::ifstream &textFile, const std::string &targetSection, sf::
     return text;
 }
 
+// fix for the stupid swedish letter bullshit
+sf::String UTF8(const char* str)
+{
+    return sf::String::fromUtf8(
+        str,
+        str + std::strlen(str)
+    );
+}
+
 // function for poll events (required by sfml to run every frame)
-void pollEvents(sf::RenderWindow &window, GameState &state, sf::String &userInput, sf::Text &inputText)
+void pollEvents(sf::RenderWindow &window, sf::String &userInput, sf::Text &inputText, std::vector<std::vector<command>> &commands, int &currentSection, sf::String &list, sf::Text &tutorialText)
 {
     while (const std::optional event = window.pollEvent())
     {
@@ -50,10 +65,13 @@ void pollEvents(sf::RenderWindow &window, GameState &state, sf::String &userInpu
             window.close();
         }
 
-        // if key pressed during launch screen then switch to playing state
-        if (state == GameState::LaunchScreen && event->is<sf::Event::KeyPressed>())
+        // close window if escape is pressed
+        if (event->is<sf::Event::KeyPressed>())
         {
-            state = GameState::Playing;
+            if (event->getIf<sf::Event::KeyPressed>()->code == sf::Keyboard::Key::Escape)
+            {
+                window.close();
+            }
         }
 
         // text input box
@@ -73,7 +91,28 @@ void pollEvents(sf::RenderWindow &window, GameState &state, sf::String &userInpu
                 userInput += unicode;
             }
 
-            inputText.setString(sf::String::fromUtf8(userInput.begin(), userInput.end()));
+            inputText.setString(userInput);
+        }
+
+        if (event->is<sf::Event::KeyPressed>() && event->getIf<sf::Event::KeyPressed>()->code == sf::Keyboard::Key::Enter)
+        {
+
+            for (const auto& cmd : commands[currentSection])
+            {
+                if (userInput == cmd.text)
+                {
+                    currentSection = cmd.SectionNumber;
+                    break;
+                }
+            }   
+
+            list = UTF8("Kommandon: ");
+            for (const auto& cmd : commands[currentSection])
+                list += cmd.text + UTF8(". ");
+
+            tutorialText.setString(list);
+            userInput.clear();
+            inputText.setString("");
         }
     }
 }
@@ -87,36 +126,59 @@ int main()
     float scaleY = 1200 / 180.f;
 
     // get text file
-    std::ifstream textFile("texts/main.txt");
+    std::ifstream textFile("assets/texts/main.txt");
 
     // sections of the game
     std::vector<std::string> sections =
-    {"intro", "fackla-väggen", "dead end"};
+    {"intro", "fackla-väggen", "stup", "mörk-hall", "stup-hoppa",};
 
-    // sets gamestate to the launch screen
-    GameState state = GameState::LaunchScreen;
+    std::vector<std::vector<command>> commands =
+    {
+        // section 0, start
+        {
+            {UTF8("gå framåt"), 1}
+        },
+        // section 1, höger/vänster
+        {
+            {UTF8("gå åt höger"), 2},
+            {UTF8("gå åt vänster"), 3}
+        },
+        // section 2, stup
+        {
+            {UTF8("hoppa"), 4},
+            {UTF8("gå tillbaks"), 1}
+        },
+        // section 3, mörk korridor
+        {
+            {UTF8("bajsa ner dig"), 6},
+            {UTF8("bajsa ner dig2"), 7}
+        }
+    };
 
     // renders window
     sf::RenderWindow window(sf::VideoMode({720, 540}), "game");
     
-    // ui + background sprite
-    sf::Texture ui("sprites/ui.png");
+    // ui background sprite
+    sf::Texture ui("assets/sprites/ui.png");
     sf::Sprite uiSprite(ui);
+
+    sf::Texture jumpscare("assets/sprites/face.png");
+    sf::Sprite face(jumpscare);
 
     // cursor sprite
     sf::RectangleShape cursor;
-    cursor.setSize({4 * scaleX, 14 * scaleY});
+    cursor.setSize({4 * scaleX, 6 * scaleY});
 
     // self explanitory text stuff
     sf::Font font;
-    if (!font.openFromFile("fonts/pixeloid-font/normal.ttf"))
+    if (!font.openFromFile("assets/fonts/pixeloid-font/normal.ttf"))
     {
         std::cerr << "Failed to load font" << std::endl;
         return -1;
     }
     // bold font
     sf::Font fontBold;
-    if (!fontBold.openFromFile("fonts/pixeloid-font/bold.ttf"))
+    if (!fontBold.openFromFile("assets/fonts/pixeloid-font/bold.ttf"))
     {
         std::cerr << "Failed to load font" << std::endl;
         return -1;
@@ -125,9 +187,17 @@ int main()
     // user input string and text
     sf::String userInput;
     sf::Text inputText(font, "", 40);
-    inputText;
+    inputText.setPosition({175, 1035});
+
+    sf::String list(UTF8("Kommandon: gå framåt."));
+    sf::Text tutorialText(font, list, 40);
+    tutorialText.setPosition({20 * scaleX, 800});
+
+    sf::Text sectionText(font, "", 40);
 
     sf::Clock cursorTimer;
+    sf::Clock textCheckTimer;
+    sf::Clock faceTimer;
 
     bool cursorVisible = true;
     int currentSection = 0;
@@ -135,7 +205,7 @@ int main()
     // main loop
     while (window.isOpen())
     {
-        pollEvents(window, state, userInput, inputText);
+        pollEvents(window, userInput, inputText, commands, currentSection, list, tutorialText);
         
         // if windowed then fullscreen
         if (window.getSize().x != sf::VideoMode::getDesktopMode().size.x)
@@ -146,22 +216,31 @@ int main()
         }
 
         // main game logic
-        
         cursor.setPosition({inputText.getPosition().x + inputText.getLocalBounds().size.x, inputText.getPosition().y});
 
+        // cursor blonk
         if (cursorTimer.getElapsedTime().asSeconds() >= 0.5)
         {
             cursorVisible = !cursorVisible;
             cursorTimer.restart();
         }
 
-        static sf::Text sectionText = getText(textFile, sections[currentSection], font);
+        // 
+
+        // delay for text so it wont check every frame or hang itself completely (you little fucker fuck you) 
+        if (textCheckTimer.getElapsedTime().asSeconds() >= 0.05)
+        {
+            sectionText = getText(textFile, sections[currentSection], font);
+            textCheckTimer.restart();
+        }
+
+        // screen text
         window.clear();
         window.draw(uiSprite);
         window.draw(sectionText);
         window.draw(inputText);
+        window.draw(tutorialText);
         if (cursorVisible) window.draw(cursor);
-        
 
         window.display();
     }
